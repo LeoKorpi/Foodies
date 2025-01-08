55e0b10a7095e7c0aae2a78c57e90a03c4e85397
LeoKorpi <leokorpi.game@gmail.com>
2025-01-08 13:11:21 +0100

    added pipe to truncate item descriptions down to 60 characters

---
+++ src/app/shared/truncate.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'truncate',
+})
+export class TruncatePipe implements PipeTransform {
+  transform(value: string, limit: number = 57): string {
+    if (!value || value.length <= limit) return value;
+    return value.substring(0, limit) + '...';
+  }
+}
