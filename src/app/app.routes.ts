9c13292544aa62701a0b0168959ee7cbb81f5043
LeoKorpi <leokorpi.game@gmail.com>
2025-01-10 17:45:07 +0100

    added routing to detailed view of recipe

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
+import { RecipeDetailsComponent } from './components/recipe-details/recipe-details.component';
 
-export const routes: Routes = [{ path: '', component: HomeComponent }];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'recipe/:id', component: RecipeDetailsComponent },
+];
--- src/app/components/recipe-item/recipe-item.component.ts
+++ src/app/components/recipe-item/recipe-item.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { CheckForAuthorPipe } from '../../shared/check-for-author.pipe';
 import { Recipe } from '../../shared/interfaces';
 import { TruncatePipe } from '../../shared/truncate.pipe';
-import { UppercaseAuthorsPipe } from '../../shared/uppercase-authors.pipe';
+import { JoinAuthorsPipe } from '../../shared/join-authors.pipe';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-item',
-  imports: [TruncatePipe, CheckForAuthorPipe, UppercaseAuthorsPipe],
+  imports: [TruncatePipe, CheckForAuthorPipe, JoinAuthorsPipe, RouterLink],
   templateUrl: './recipe-item.component.html',
   styleUrl: './recipe-item.component.css',
 })
