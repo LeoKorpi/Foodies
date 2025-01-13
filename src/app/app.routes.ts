7d0eab3b825322a9711c2e4f379ef803011088be
LeoKorpi <leokorpi.game@gmail.com>
2025-01-13 15:26:39 +0100

    added about me page & routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { RecipeDetailsComponent } from './components/recipe-details/recipe-details.component';
 import { CreatePostComponent } from './views/create-post/create-post.component';
+import { AboutMeComponent } from './views/about-me/about-me.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'create-post', component: CreatePostComponent },
+  { path: 'about-me', component: AboutMeComponent },
   { path: 'recipe/:id', component: RecipeDetailsComponent },
 ];
