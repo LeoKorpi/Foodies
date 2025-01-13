ba9b5fda8d3f39a1602c33656e0790cbe3c5a3fc
LeoKorpi <leokorpi.game@gmail.com>
2025-01-13 11:06:24 +0100

    added routing to create-post component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { RecipeDetailsComponent } from './components/recipe-details/recipe-details.component';
+import { CreatePostComponent } from './views/create-post/create-post.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'create-post', component: CreatePostComponent },
   { path: 'recipe/:id', component: RecipeDetailsComponent },
 ];
