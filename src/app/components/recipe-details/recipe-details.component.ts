f7ccc0a651668a987b2d73732166500941a9068c
LeoKorpi <leokorpi.game@gmail.com>
2025-01-14 13:38:06 +0100

    added a signal to track id from url

--- src/app/components/recipe-details/recipe-details.component.ts
+++ src/app/components/recipe-details/recipe-details.component.ts
@@ -1,24 +1,24 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { ActivatedRoute, RouterLink } from '@angular/router';
-import { CheckForAuthorPipe } from '../../shared/pipes/check-for-author.pipe';
+import { Component, computed } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { CommentsService } from '../../shared/comments.service';
 import {
+  Comment,
   Ingredient,
   Instruction,
   Recipe,
-  Comment,
 } from '../../shared/interfaces';
-import { JoinAuthorsPipe } from '../../shared/pipes/join-authors.pipe';
-import { RecipesService } from '../../shared/recipes.service';
+import { CheckForAuthorPipe } from '../../shared/pipes/check-for-author.pipe';
 import { DateFormatterPipe } from '../../shared/pipes/date-formatter.pipe';
 import { HideZeroQuantityPipe } from '../../shared/pipes/hide-zero-quantity.pipe';
+import { JoinAuthorsPipe } from '../../shared/pipes/join-authors.pipe';
+import { RecipesService } from '../../shared/recipes.service';
 import { CommentItemComponent } from '../comment-item/comment-item.component';
-import { CommentsService } from '../../shared/comments.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-details',
   imports: [
-    CommonModule,
     RouterLink,
     CheckForAuthorPipe,
     JoinAuthorsPipe,
@@ -31,49 +31,49 @@ import { CommentsService } from '../../shared/comments.service';
   styleUrl: './recipe-details.component.css',
 })
 export class RecipeDetailsComponent {
-  id: number = -1;
+  id = computed(() => {
+    const paramMap = this.paramMap();
+    return paramMap ? parseInt(paramMap.get('id') || '0', 10) : null;
+  });
+
+  private paramMap!: () => ParamMap | null;
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private recipeService: RecipesService,
     private commentService: CommentsService
   ) {
-    activatedRoute.params.subscribe(
-      (params) => (this.id = parseInt(params['id']))
-    );
+    this.paramMap = toSignal(this.activatedRoute.paramMap, {
+      initialValue: null,
+    });
   }
 
   get recipe(): Recipe | undefined {
-    return this.recipeService.recipes.find((all) => all.id == this.id);
+    return this.recipeService.recipes.find((all) => all.id == this.id());
   }
 
   get ingredients(): Ingredient[] | undefined {
-    if (this.recipe) return this.recipe.ingredients;
-    return undefined;
+    return this.recipe?.ingredients;
   }
 
   get instructions(): Instruction[] | undefined {
-    if (this.recipe) return this.recipe.instructions;
-    return undefined;
+    return this.recipe?.instructions;
   }
 
   get comments(): Comment[] | undefined {
-    if (this.recipe) return this.commentService.comments;
-    return undefined;
+    return this.recipe ? this.commentService.comments : undefined;
   }
 
+  // flytta till recipe.service för att hålla all data-logik där
   addReaction(type: 'likes' | 'dislikes'): void {
-    if (this.recipe !== undefined) {
-      this.recipe[type] += 1;
+    if (this.recipe) {
+      this.recipe[type]++;
       localStorage.setItem(
         'recipes',
         JSON.stringify(
-          this.recipeService.recipes.map((all) => {
-            if (this.recipe === undefined || all.id !== this.recipe.id) {
-              return all;
-            }
-            return this.recipe;
-          })
+          this.recipeService.recipes.map((all) =>
+            this.recipe && all.id === this.recipe.id ? this.recipe : all
+          )
         )
       );
     }
