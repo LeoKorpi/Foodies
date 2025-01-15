0bbfad207951699a8f02f1709a054426eebfb611
LeoKorpi <leokorpi.game@gmail.com>
2025-01-15 09:07:37 +0100

    added some providers for testing

--- src/app/components/recipe-item/recipe-item.component.spec.ts
+++ src/app/components/recipe-item/recipe-item.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RecipeItemComponent } from './recipe-item.component';
+import { provideRouter } from '@angular/router';
 
 describe('RecipeItemComponent', () => {
   let component: RecipeItemComponent;
@@ -8,6 +9,7 @@ describe('RecipeItemComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RecipeItemComponent],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(RecipeItemComponent);
