9c7b4ffab483f8a4ed67429a360afd3600dcd6f9
LeoKorpi <leokorpi.game@gmail.com>
2025-01-17 11:48:12 +0100

    removed unused pipes

--- src/app/components/recipe-item/recipe-item.component.ts
+++ src/app/components/recipe-item/recipe-item.component.ts
@@ -1,13 +1,10 @@
 import { Component, Input } from '@angular/core';
-import { CheckForAuthorPipe } from '../../shared/pipes/check-for-author.pipe';
 import { Recipe } from '../../shared/interfaces';
-import { TruncatePipe } from '../../shared/pipes/truncate.pipe';
-import { JoinAuthorsPipe } from '../../shared/pipes/join-authors.pipe';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-item',
-  imports: [TruncatePipe, CheckForAuthorPipe, JoinAuthorsPipe, RouterLink],
+  imports: [RouterLink],
   templateUrl: './recipe-item.component.html',
   styleUrl: './recipe-item.component.css',
 })
