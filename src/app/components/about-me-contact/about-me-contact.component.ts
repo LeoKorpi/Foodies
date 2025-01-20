7281e62dad3d88fd45080ff1421a2fc7c2ecbcaa
LeoKorpi <leokorpi.game@gmail.com>
2025-01-20 17:14:12 +0100

    added form for contacting me

--- src/app/components/about-me-contact/about-me-contact.component.ts
+++ src/app/components/about-me-contact/about-me-contact.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-about-me-contact',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './about-me-contact.component.html',
-  styleUrl: './about-me-contact.component.css'
+  styleUrl: './about-me-contact.component.css',
 })
 export class AboutMeContactComponent {
+  messageSent: boolean = false;
 
+  contactForm = new FormGroup({
+    sender: new FormControl(''),
+    email: new FormControl(''),
+    subject: new FormControl(''),
+    content: new FormControl(''),
+  });
+
+  onSubmit() {
+    if (this.contactForm.invalid)
+      return alert('All fields must be filled in before sending your message!');
+
+    const contactSubmission = {
+      sender: this.contactForm.value.sender,
+      email: this.contactForm.value.email,
+      subject: this.contactForm.value.subject,
+      content: this.contactForm.value.content,
+    };
+    console.warn('Someone has contacted you! ', contactSubmission);
+    this.messageSent = true;
+  }
 }
