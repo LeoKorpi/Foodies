21e9c1a8bdb90d72f5acae9fe4f4dc240cbf739b
LeoKorpi <leokorpi.game@gmail.com>
2025-01-21 23:12:53 +0100

    added styling to about me page

--- src/app/components/about-me-info/about-me-info.component.ts
+++ src/app/components/about-me-info/about-me-info.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { IconEmoticonComponent } from '../icons/icon-emoticon.component';
 
 @Component({
   selector: 'app-about-me-info',
-  imports: [],
+  imports: [IconEmoticonComponent],
   templateUrl: './about-me-info.component.html',
   styleUrl: './about-me-info.component.css',
 })
